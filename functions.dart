7e9cce82a6297d58ff24f1c3e8bbcda0247d83b3
Syed Osama Ahmed <batch17.026@gmail.com>
2022-11-21 14:03:47 +0500

    learnt sets,maps,nullable safety,nullable assignment operator dart

--- functions.dart
+++ functions.dart
@@ -8,13 +8,20 @@ double addTwoNumbers(double num1, double num2) {
   return num1 + num2;
 }
 
+String promptUser(String promptText) {
+  print(promptText);
+  return stdin.readLineSync()!;
+}
+
 void main() {
   String? username;
+  String color = promptUser('What is your favourite color?');
+  print('the color is :$color');
 
   print("Enter name:");
   username = stdin.readLineSync()!;
 
-  // greetUser(username);
+  greetUser(username);
 
   print(addTwoNumbers(8, 13));
 }
